e1999e38e363bbe3da31802cba13428a1ea32ae9
Alexander-Lipatov <lap1993.12@yandex.ru>
2021-09-11 14:43:08 +0300

    добавил немного кода в файл main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello world!')
+import time
+
+
+def printCycle(a):
+    while a != 10:
+        print("I'm the loop")
+        time.sleep(1)
+        a = a + 1
+        if a == 10:
+            break
+
+printCycle(1)
